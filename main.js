61d304d77e8e371cde54c0812e6f40afb548e1cf
EJ <ejg132@gmail.com>
2019-09-16 13:43:20 -0700

    Fixed hamburger

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 'use strict';
 
 function hamburgerShow() {
-    $('.menu-wrap').on('click', function (event) {
-        $('.menu').toggleClass('invis');
+
+    $('.menu-wrap').on('click', function(){
+
+
+        console.log($('input[type=checkbox]'))
+
+        if ($('.toggler').prop('checked') == false) {
+        // $('.toggler').prop('checked', true)
+        $('#show').remove()
+    }
+
+        if ($('.toggler').prop('checked') == true) {
+        // $('.toggler').prop('checked', true)
+        $('head').append('<link rel="stylesheet" type="text/css" id="show" href="activeHamburger.css">')
+    }
+
     })
+
+    $('#uncheck').on('click', function () {
+        $('.toggler').prop('checked', false)
+    })
+
 }
 
 
